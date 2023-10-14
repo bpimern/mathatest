03df116d29ee4ce1c4cae0376fda726ea71af27b
Muntasir12 <mixono6437@v2ssr.com>
2023-10-14 11:40:51 +0600

    initial commit

---
+++ src/Constants/data.ts
@@ -0,0 +1,16 @@
+export const navigation = [
+    {title:"Hoem",href:"/"},
+    {title:"About",href:"/about"},
+    {title:"Services",href:"/services"},
+    {title:"Protfolio",href:"/protfolio"},
+    {title:"Contact",href:"/contact"},
+    {title:"Resume",href:"/resume"},
+];
+export const Services = [
+    {title:"Web Design",},
+    {title:"Web Development"},
+    {title:"Learn Web Development",},
+    {title:"MERN Stack Development",},
+    {title:"Creating Artical",},
+];
+
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Banner from "@/Components/Banner";
+import Container from "@/Components/Container";
+import Services from "@/Components/Services";
+import { Caladea } from "next/font/google";
+
+
+
+export default function Home() {
+  return (
+    < >
+      
+      <Banner/>
+      <Services/>
+    </>
+  );
+}
